567ae783b6e2d800fdc821938b6e29e3697ed52b
Daniel Kimani <kimdan2030@gmail.com>
2025-09-17 07:34:37 +0300

    Logs

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Database\Eloquent\Model;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -25,7 +26,34 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+            // Global model listeners
+    Model::created(function ($model) {
+        if (auth()->check()) {
+            activity()
+                ->causedBy(auth()->user())
+                ->performedOn($model)
+                ->log("Created " . class_basename($model));
+        }
+    });
+
+    Model::updated(function ($model) {
+        if (auth()->check() && !empty($model->getChanges())) {
+            activity()
+                ->causedBy(auth()->user())
+                ->performedOn($model)
+                ->withProperties(['changes' => $model->getChanges()])
+                ->log("Updated " . class_basename($model));
+        }
+    });
+
+    Model::deleted(function ($model) {
+        if (auth()->check()) {
+            activity()
+                ->causedBy(auth()->user())
+                ->performedOn($model)
+                ->log("Deleted " . class_basename($model));
+        }
+    });
     }
 
     /**
